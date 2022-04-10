6f299b7023c2c896d3d762a898df60f79b9c5fc8
Anıl <cazorte@gmail.com>
2022-04-11 02:30:59 +0300

    first attend

---
+++ day39_Recap/shapeTask/ShapeObjects.java
@@ -0,0 +1,15 @@
+package day39_Recap.shapeTask;
+
+public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Square s1 = new Square(5);
+        System.out.println(s1);
+
+
+
+    }
+
+
+}
