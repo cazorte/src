d2547a43871b92cdb7f52e2d6038671957380076
Anıl <cazorte@gmail.com>
2022-04-14 02:38:27 +0300

    first attend

---
+++ day40_FinalKeyword/EncapsulationReview.java
@@ -0,0 +1,14 @@
+package day40_FinalKeyword;
+
+import day39_Recap.shapeTask.Circle;
+
+public class EncapsulationReview {
+
+    protected static Circle circle;
+    static Circle circle1;
+
+
+
+
+
+}
---
+++ utilities/AccessModifiers_Test3.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import day40_FinalKeyword.EncapsulationReview;
+
+public class AccessModifiers_Test3 extends EncapsulationReview {
+
+    public static void main(String[] args) {
+
+        System.out.println(circle);
+    }
+
+
+
+
+}
