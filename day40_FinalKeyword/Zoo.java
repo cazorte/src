34765837a5a04e46fba93835d1934d6d4e2f5d62
Anıl <cazorte@gmail.com>
2022-04-19 01:34:45 +0300

    first attend

--- day40_FinalKeyword/Zoo.java
+++ day40_FinalKeyword/Zoo.java
@@ -5,11 +5,22 @@ public class Zoo {
     public static void main(String[] args) {
 
         Dog dog = new Dog("Max", "Husky", 'M', "White", "Large", 4);
+        Dog dog1 = new Dog("Max", "Husky1", 'M', "White", "Large", 4);
+        Dog dog2 = new Dog("Max", "Husky2", 'M', "White", "Large", 4);
 
         System.out.println(dog.getBreed());
         System.out.println(dog.getGender());
 
         dog.drink();
+
+
+        System.out.println(dog.getBreed());
+        System.out.println(dog1.getBreed());
+        System.out.println(dog2.getBreed());
+
+
+
+        //01.00 da kaldım
 
 
 
